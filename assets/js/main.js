2c9918b123c7b51f2f807a9bc76323c03a103324
Nadine Ju <nadine.jura@online.de>
2020-04-24 23:00:56 -0700

    general styles and footer added

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    /********************************************
+     FOOTER YEAR
+    ********************************************/
+    const year = new Date().getFullYear();
+    
+    document.getElementById('year').innerHTML = year;
+    
+    /********************************************
+     TESTIMONIALS
+    ********************************************/
+    
+    });
