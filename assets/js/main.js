e0c6a85949fcdabaa5f975006aa117bbd4d6913b
Nadine Ju <nadine.jura@online.de>
2020-05-18 17:42:02 -0700

    addind stylesheets for various screen sizes

--- assets/js/main.js
+++ assets/js/main.js
@@ -108,4 +108,37 @@ $(document).ready(function () {
             $(allQuotes[allQuotes.length - 1]).addClass('left');
         }
     };
+
+
+    $('.responsive-carousel').slick({
+        dots: true,
+        arrows: false,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+            },
+            {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+            },
+            {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+            }]
+    });
 });
