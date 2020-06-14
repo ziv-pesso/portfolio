03d48f8f5571f49a294844b5255032ab701eee8a
Nadine Ju <nadine.jura@online.de>
2020-06-14 20:32:19 +0200

    image modal works

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,20 +1,19 @@
 $(document).ready(function() {
-$('.personal-content-img').on('click', function() {
-    // console.log('clicked');
-    // const modalId = $(this).attr('data-id');
-    const caption = $(this).attr('data-alt');
-    const src = $(this).attr('data-src');
+    $('.personal-content-img').on('click', function() {
+        console.log('clicked');
+        const caption = $(this).attr('data-alt');
+        const src = $(this).attr('data-src');
 
-    const img = $(`<img class="zoomed-image" src=${src} alt=${caption}>`); 
+        const img = $(`<img class="zoomed-image" src=${src} alt=${caption}>`); 
 
-    $('#modal-content').append(img);
-    $('#modal').show();
+        $('#modal').append(img);
+        // $('#modal-content').append(img);
+        $('#modal').addClass('active');
 
-    $('.close').on('click', function() {
-        const img = $('.zoomed-image');
-        img.remove();
-        $('#modal').hide();
+        $('.close').on('click', function() {
+            const img = $('.zoomed-image');
+            img.remove();
+            $('#modal').removeClass('active');
+        });
     });
-});
-
 });
